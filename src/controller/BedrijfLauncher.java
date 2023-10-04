cc7a6d71b9e396688c0905a30c349192f12fdeda
Remco Ketting <remmychainiac@hotmail.com>
2023-10-04 11:27:03 +0200

    Dagdeel 10!!!

--- src/controller/BedrijfLauncher.java
+++ src/controller/BedrijfLauncher.java
@@ -1,5 +1,7 @@
 package controller;
 
+import database.AfdelingDAO;
+import database.DBaccess;
 import model.*;
 
 import java.io.File;
@@ -17,95 +19,114 @@ public class BedrijfLauncher {
 
   public static void main(String[] args) {
 
-    ArrayList<Afdeling> afdelingen = new ArrayList<>();
+//    AfdelingDAO inpropper = new AfdelingDAO();
+//
+//    inpropper.slaAfdelingOp(new Afdeling("HR", "Hilversum"));
 
-    File leesbestand = new File("resources/Afdelingen.txt");
+    geefAfdelingenMetPlaats("Amsterdam");
 
-    try (Scanner bestandsvreter = new Scanner(leesbestand)){
-      while (bestandsvreter.hasNextLine()){
-        afdelingen.add(new Afdeling(bestandsvreter.next(), bestandsvreter.next()));
-      }
-    } catch (FileNotFoundException geenbestand) {
-      System.out.println("Woepsie, geen bestand :(");
-    }
-
-    ArrayList<Persoon> personen = new ArrayList<>();
-
-    ArrayList<String> bestandsregels = new ArrayList<>();
-
-    File personenbestand = new File("resources/Personen.csv");
-
-    try (Scanner bestandtsjapper = new Scanner(personenbestand)){
-      while (bestandtsjapper.hasNextLine()){
-        bestandsregels.add(bestandtsjapper.nextLine());
-
-      }
-
-    } catch (FileNotFoundException nichtsda) {
-      System.out.println("Waah, geen bestand!");
-    }
-
-      for (String bestandsregel : bestandsregels) {
-          String[] persoonrecept = bestandsregel.split(",");
-          if (persoonrecept[0].equals("Werknemer")) {
-              personen.add(new Werknemer(
-                      persoonrecept[1],
-                      persoonrecept[2],
-                      afdelingen.get(Integer.parseInt(persoonrecept[3])),
-                      Double.parseDouble(persoonrecept[4])
-              ));
-          } else if (persoonrecept[0].equals("Zzper")) {
-              personen.add(new ZZPer(
-                      persoonrecept[1],
-                      persoonrecept[2],
-                      afdelingen.get(Integer.parseInt(persoonrecept[3])),
-                      Double.parseDouble(persoonrecept[4])
-              ));
-          } else {
-              personen.add(new Vrijwilliger(
-                      persoonrecept[1],
-                      persoonrecept[2],
-                      afdelingen.get(Integer.parseInt(persoonrecept[3]))));
-
-          }
-      }
-    Collections.sort(personen);
-
-    for (Persoon persoon : personen) {
-      System.out.println(persoon);
-    }
-    System.out.println();
-
-    File personenPerAfdeling = new File("resources/PersonenPerAfdeling.txt");
-    try (PrintWriter schrijver = new PrintWriter(personenPerAfdeling)){
-      for (Afdeling afdeling : afdelingen) {
-        schrijver.printf("Afdeling: %s\n", afdeling.getAfdelingsNaam());
-        for (Persoon persoon : personen) {
-          if (persoon.getAfdeling().equals(afdeling)){
-            schrijver.println("--" + persoon);
-          }
-
-        }
-        schrijver.println();
-      }
-    } catch (FileNotFoundException e) {
-      System.out.println("Oi, kan niet schrijven!");
-    }
-
-    for (Afdeling afdeling : afdelingen) {
-      System.out.printf("Afdeling: %s\n", afdeling.getAfdelingsNaam());
-      for (Persoon persoon : personen) {
-        if (persoon.getAfdeling().equals(afdeling)){
-          System.out.println("--" + persoon);
-        }
-
-      }
-      System.out.println();
-    }
+//    ArrayList<Afdeling> afdelingen = new ArrayList<>();
+//
+//    File leesbestand = new File("resources/Afdelingen.txt");
+//
+//    try (Scanner bestandsvreter = new Scanner(leesbestand)){
+//      while (bestandsvreter.hasNextLine()){
+//        afdelingen.add(new Afdeling(bestandsvreter.next(), bestandsvreter.next()));
+//      }
+//    } catch (FileNotFoundException geenbestand) {
+//      System.out.println("Woepsie, geen bestand :(");
+//    }
+//
+//    ArrayList<Persoon> personen = new ArrayList<>();
+//
+//    ArrayList<String> bestandsregels = new ArrayList<>();
+//
+//    File personenbestand = new File("resources/Personen.csv");
+//
+//    try (Scanner bestandtsjapper = new Scanner(personenbestand)){
+//      while (bestandtsjapper.hasNextLine()){
+//        bestandsregels.add(bestandtsjapper.nextLine());
+//
+//      }
+//
+//    } catch (FileNotFoundException nichtsda) {
+//      System.out.println("Waah, geen bestand!");
+//    }
+//
+//      for (String bestandsregel : bestandsregels) {
+//          String[] persoonrecept = bestandsregel.split(",");
+//          if (persoonrecept[0].equals("Werknemer")) {
+//              personen.add(new Werknemer(
+//                      persoonrecept[1],
+//                      persoonrecept[2],
+//                      afdelingen.get(Integer.parseInt(persoonrecept[3])),
+//                      Double.parseDouble(persoonrecept[4])
+//              ));
+//          } else if (persoonrecept[0].equals("Zzper")) {
+//              personen.add(new ZZPer(
+//                      persoonrecept[1],
+//                      persoonrecept[2],
+//                      afdelingen.get(Integer.parseInt(persoonrecept[3])),
+//                      Double.parseDouble(persoonrecept[4])
+//              ));
+//          } else {
+//              personen.add(new Vrijwilliger(
+//                      persoonrecept[1],
+//                      persoonrecept[2],
+//                      afdelingen.get(Integer.parseInt(persoonrecept[3]))));
+//
+//          }
+//      }
+//    Collections.sort(personen);
+//
+//    for (Persoon persoon : personen) {
+//      System.out.println(persoon);
+//    }
+//    System.out.println();
+//
+//    File personenPerAfdeling = new File("resources/PersonenPerAfdeling.txt");
+//    try (PrintWriter schrijver = new PrintWriter(personenPerAfdeling)){
+//      for (Afdeling afdeling : afdelingen) {
+//        schrijver.printf("Afdeling: %s\n", afdeling.getAfdelingsNaam());
+//        for (Persoon persoon : personen) {
+//          if (persoon.getAfdeling().equals(afdeling)){
+//            schrijver.println("--" + persoon);
+//          }
+//
+//        }
+//        schrijver.println();
+//      }
+//    } catch (FileNotFoundException e) {
+//      System.out.println("Oi, kan niet schrijven!");
+//    }
+//
+//    for (Afdeling afdeling : afdelingen) {
+//      System.out.printf("Afdeling: %s\n", afdeling.getAfdelingsNaam());
+//      for (Persoon persoon : personen) {
+//        if (persoon.getAfdeling().equals(afdeling)){
+//          System.out.println("--" + persoon);
+//        }
+//
+//      }
+//      System.out.println();
+//    }
 
   }
   public static void toonJaarinkomen(Persoon persoon){
     System.out.printf("%s verdient %.2f per jaar.\n", persoon.getNaam(), persoon.berekenJaarInkomen());
+  }
+  public static void geefAfdelingenMetPlaats(String plaats){
+    AfdelingDAO zuiger = new AfdelingDAO();
+
+    ArrayList<Afdeling> afdelingen = zuiger.leesAfdelingen();
+
+    for (Afdeling afdeling : afdelingen) {
+      if (afdeling.getAfdelingsPlaats().equals(plaats)){
+        System.out.printf("Afdeling: %s\n", afdeling);
+      }
+
+    }
+
   }
 
 
---
+++ src/database/AfdelingDAO.java
@@ -0,0 +1,77 @@
+package database;
+
+import model.Afdeling;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+/**
+ * Author: Remco Ketting
+ * Purpose of program:
+ */
+public class AfdelingDAO {
+
+
+    public void slaAfdelingOp(Afdeling afdeling){
+
+        String afdelingsnaam = afdeling.getAfdelingsNaam();
+        String afdelingsplaats = afdeling.getAfdelingsPlaats();
+
+        DBaccess toegang = new DBaccess("Bedrijf", "userBedrijf", "userBedrijfPW");
+
+        toegang.openConnection();
+
+        Connection verbinding = toegang.getConnection();
+
+        String sql2 = "INSERT INTO afdeling VALUES(?, ?);";
+        try {
+            PreparedStatement preparedStatement =
+                    toegang.getConnection().prepareStatement(sql2);
+            preparedStatement.setString(1, afdelingsnaam);
+            preparedStatement.setString(2, afdelingsplaats);
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException sqlFout) {
+            System.out.println(sqlFout);
+        }
+
+
+        toegang.closeConnection();
+
+    }
+    public ArrayList<Afdeling> leesAfdelingen(){
+        DBaccess toegang = new DBaccess("Bedrijf", "userBedrijf", "userBedrijfPW");
+
+        toegang.openConnection();
+
+        Connection verbinding = toegang.getConnection();
+
+        ArrayList<Afdeling> returnlist = new ArrayList<>();
+
+        if (verbinding != null) {
+            System.out.println("De verbinding is gemaakt!");
+            String sql = "SELECT * FROM afdeling;";
+            try {
+                PreparedStatement preparedStatement = verbinding.prepareStatement(sql);
+                ResultSet resultaat = preparedStatement.executeQuery();
+                while (resultaat.next()){
+                    String naam = resultaat.getString("afdelingsnaam");
+                    String plaats = resultaat.getString("afdelingsplaats");
+                    returnlist.add(new Afdeling(naam, plaats));
+                }
+
+
+
+                verbinding.close();
+            } catch (SQLException sqlFout) {
+                System.out.println(sqlFout);
+            }
+        }
+
+        return returnlist;
+
+
+    }
+
+
+}
